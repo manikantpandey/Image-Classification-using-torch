c0207ce4b6b80711ce86beeb55e8b3e25667fef6
Mani kant Pandey <96922351+manikantpandey@users.noreply.github.com>
2024-04-06 23:27:37 +0530

    init

--- traning.py
+++ traning.py
@@ -1,5 +1,3 @@
-# Importing  the required libraries
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -7,48 +5,86 @@ import torch.utils
 import torch.utils.data
 from torchvision import datasets, transforms, models
 import os
-# Define data transformations 
 
-data_transform= {
-    'train' : transforms.Compose([
+# Define data transformations
+data_transform = {
+    'train': transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
-    'valid' : transforms.Compose([
+    'val': transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToTensor( ),
+        transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])    
+    ])
 }
 
 # Define Data directory
-data_dir= 'dataset'
+data_dir = 'dataset'
 
-# Data Loders
-image_datasets = {x: datasets.Imagefolder(os.path.joins(data_dir,x),data_transform[x])for x in ['train','val']}
-dataloaders= {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4)for x in ['train','val']}
-class_names= image_datasets['train'].classes
+# Function to train the model
+def train_model():
+    # Data Loaders
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transform[x]) for x in ['train', 'val']}
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'val']}
+    dataset_size = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
 
+    # Load the pre-trained ResNet-18 model
+    model = models.resnet18(pretrained=True)
 
-# Load the pre-trained ResNet-18 model
-model=models.resnet18(pretrained=True)
+    for name, param in model.named_parameters():
+        if "fc" in name:
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
 
-for name, param in model.named_parameters():
-    if "fc" in name:
-        param.requires_grad=True
-    else:
-        param.requires_grad=False
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-criterion= nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
 
-device= torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model=model.to(device)
+    # Training loop
+    num_epochs = 10
+    for epoch in range(num_epochs):
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
 
+            running_loss = 0.0
+            running_corrects = 0
 
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                optimizer.zero_grad()
 
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
 
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
 
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_size[phase]
+            epoch_acc = running_corrects.double() / dataset_size[phase]
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+    print("Training complete.")
+
+    # Save the model
+    torch.save(model.state_dict(), "flower_classification_model.pth")
+
+if __name__ == "__main__":
+    train_model()
